5ab6c1aab61681b144a27fd3d04e6f6bb24b4417
Vitali Matusevich <vimatu@gmail.com>
2020-08-11 23:24:53 +0300

    feat: add action next_step

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,7 +1,13 @@
-import { NEXT_STEP } from "./types";
+import { NEXT_STEP, GET_RANDOM_BIRD } from "./types";
 
 export function nextStep() {
   return {
     type: NEXT_STEP,
+  }
+}
+
+export function getRandomBird() {
+  return {
+    type: GET_RANDOM_BIRD,
   }
 }
--- src/redux/types.js
+++ src/redux/types.js
@@ -1,1 +1,2 @@
 export const NEXT_STEP = 'GAME/NEXT_STEP';
+export const GET_RANDOM_BIRD = 'GAME/GET_RANDOM_BIRD';
